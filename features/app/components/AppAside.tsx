7d6751900495a67b57e28f443f57315c28d9307a
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-09-09 19:11:25 +0300

    Fix sort type for aside comments

--- features/app/components/AppAside.tsx
+++ features/app/components/AppAside.tsx
@@ -2,9 +2,10 @@ import { Box } from "@mantine/core";
 import { CommentList } from "features/comment/components/CommentList";
 import { useCommentList } from "features/comment/hooks/useCommentList";
 import { FC } from "react";
+import { CommentSortType } from "ujournal-lemmy-js-client";
 
 export const AppAside: FC = () => {
-  const { data, isLoading } = useCommentList();
+  const { data, isLoading } = useCommentList({ sort: CommentSortType.New });
 
   return (
     <Box p="sm">
