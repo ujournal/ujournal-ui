f38288fd1961f34b87fcf23c5a631a7cd220ae9c
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-09-09 22:06:42 +0300

    Update DateFormatted

--- baza/components/DeteFormatted.tsx
+++ baza/components/DeteFormatted.tsx
@@ -7,43 +7,44 @@ import { useTranslation } from "react-i18next";
 import i18n from "i18next";
 
 export const DateFormatted: FC<{ date: Date }> = ({ date }) => {
-    const {t} = useTranslation();
-    const [displayFull, setDisplayFull] = useState<boolean>(false);
+  const { t } = useTranslation();
+  const [displayFull, setDisplayFull] = useState<boolean>(false);
 
-    const publishInterval = useMemo(
-        () => {
-            const {days = 0, hours = 0, minutes = 0} =
-                intervalToDuration({
-                    start: date,
-                    end: new Date(),
-                });
-            
-            const rtf = new Intl.RelativeTimeFormat(i18n.language, { style: "short" });
-            
-            return displayFull
-                ? date.toLocaleString()
-                : days > 0
-                    ? rtf.format(days * -1, "day")
-                    : hours > 0
-                        ? rtf.format(hours * -1, "hour")
-                        : rtf.format(minutes * -1, "minute");
-        }, [date]
-    );
+  const publishInterval = useMemo(() => {
+    const {
+      days = 0,
+      hours = 0,
+      minutes = 0,
+    } = intervalToDuration({
+      start: date,
+      end: new Date(),
+    });
 
-    const toggleDisplayFull = useCallback(() => {
-        setDisplayFull((displayFull) => !displayFull);
-    }, []);
+    const rtf = new Intl.RelativeTimeFormat(i18n.language, { style: "short" });
 
-    return (
-        <Tooltip label={date.toLocaleString()} openDelay={1000}>
-            <Text
-                sx={{whiteSpace: "nowrap"}}
-                color="gray"
-                size="sm"
-                onClick={toggleDisplayFull}
-            >
-                {publishInterval}
-            </Text>
-        </Tooltip>
-    );
+    return displayFull
+      ? date.toLocaleString()
+      : days > 0
+      ? rtf.format(days * -1, "day")
+      : hours > 0
+      ? rtf.format(hours * -1, "hour")
+      : rtf.format(minutes * -1, "minute");
+  }, [date, displayFull]);
+
+  const toggleDisplayFull = useCallback(() => {
+    setDisplayFull((displayFull) => !displayFull);
+  }, []);
+
+  return (
+    <Tooltip label={date.toLocaleString()} openDelay={1000}>
+      <Text
+        sx={{ whiteSpace: "nowrap" }}
+        color="gray"
+        size="sm"
+        onClick={toggleDisplayFull}
+      >
+        {publishInterval}
+      </Text>
+    </Tooltip>
+  );
 };
