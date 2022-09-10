fce0503c0942448fddd7e9f6878e30b57dbbc9b1
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-09-10 11:20:56 +0300

    Update comment loader

--- features/comment/components/CommentListLoader.tsx
+++ features/comment/components/CommentListLoader.tsx
@@ -1,12 +1,12 @@
-import { Skeleton } from "@mantine/core";
+import { Skeleton, Stack } from "@mantine/core";
 import { FC } from "react";
 
 export const CommentListLoader: FC = () => {
   return (
-    <>
+    <Stack spacing="sm">
       {Array.from(Array(3).keys()).map((key) => (
         <Skeleton height={40} key={key} />
       ))}
-    </>
+    </Stack>
   );
 };
