b4f832f3748af01c3e01bb42dfd83e12bab60a0f
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-09-12 20:19:18 +0300

    Add BASE_PATCH

--- environment.d.ts
+++ environment.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    BASE_PATCH: string | undefined;
     NEXT_PUBLIC_LEMMY_API_URL: string;
     NEXT_PUBLIC_PICTRS_API_URL: string;
     NEXT_PUBLIC_LEMMY_API_PROXY_URL: string;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.BASE_PATCH,
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
