0811533be511d2567a63ac453d4d544112f119e2
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-09-19 01:05:15 +0300

    Add accept legal button

--- features/legal/components/LegalNotification.tsx
+++ features/legal/components/LegalNotification.tsx
@@ -1,29 +1,36 @@
-import { FC, useEffect } from "react";
-import { showNotification } from "@mantine/notifications";
+import { FC, useCallback, useEffect } from "react";
+import { hideNotification, showNotification } from "@mantine/notifications";
 import Link from "next/link";
-import { Box } from "@mantine/core";
+import { Box, Button, Group } from "@mantine/core";
 
 export const LegalNotification: FC = () => {
+  const handleAccept = useCallback(() => {
+    hideNotification("legal");
+    localStorage.legalVisited = "yes";
+  }, []);
+
   useEffect(() => {
     if (!localStorage.legalVisited) {
       showNotification({
         id: "legal",
-        title: "Підтвердження правил користування вебсайтом",
         message: (
-          <>
-            Фактичне користування вебсайтом означає прийняття вами{" "}
-            <Link href={{ pathname: "/legal" }} passHref>
-              <Box component="a" sx={{ textDecoration: "underline" }}>
-                правил користування вебсайтом
-              </Box>
-            </Link>
-          </>
+          <Group noWrap align="center">
+            <Box>
+              Фактичне користування вебсайтом означає прийняття вами{" "}
+              <Link href={{ pathname: "/legal" }} passHref>
+                <Box component="a" sx={{ textDecoration: "underline" }}>
+                  правил користування вебсайтом
+                </Box>
+              </Link>
+            </Box>
+            <Button onClick={handleAccept}>Приймаю</Button>
+          </Group>
         ),
         disallowClose: true,
         autoClose: false,
       });
     }
-  }, []);
+  }, [handleAccept]);
 
   return <></>;
 };
