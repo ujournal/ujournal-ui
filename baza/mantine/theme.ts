17020abd7a65290802be1b103e755482f1f5807b
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-10-03 17:52:41 +0300

    Change font to eUkraineLight

--- baza/mantine/theme.ts
+++ baza/mantine/theme.ts
@@ -2,6 +2,6 @@ import { MantineTheme } from "@mantine/core";
 
 export const mantineTheme: Partial<MantineTheme> = {
   fontFamily:
-    "-apple-system, BlinkMacSystemFont, Roboto, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+    "eUkraineLight, -apple-system, BlinkMacSystemFont, Roboto, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
   colorScheme: "light",
 };
