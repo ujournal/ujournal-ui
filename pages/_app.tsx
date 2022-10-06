77515f1bc17c6be518f8920671ceac47272c1907
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-10-06 20:18:43 +0300

    Roll back AppMeta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,11 +14,10 @@ import { queryClient } from "baza/reactQuery";
 import { MenuToggleProvider } from "baza/hooks/useMenuToggle";
 import { AppAuthRedirect } from "features/app/components/AppAuthRedirect";
 import { LegalNotification } from "features/legal/components/LegalNotification";
+import { AppMeta } from "features/app/components/AppMeta";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { useColorSchemeLocalStorage } from "baza/hooks/useColorSchemeLocalStorage";
 import { useMemo } from "react";
-import Script from "next/script";
-import { Global } from "@mantine/core";
 
 export default function App(props: SiteAppProps) {
   const { Component, pageProps } = props;
@@ -32,75 +31,9 @@ export default function App(props: SiteAppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
-
-      <Script
-        src={`//www.googletagmanager.com/gtag/js?id=https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', '${process.env.NEXT_PUBLIC_GTAG}');
-        `}
-      </Script>
-
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-
-        <link
-          href="//fonts.googleapis.com/css?family=Roboto"
-          rel="stylesheet"
-          type="text/css"
-        />
-
-        <Global
-          styles={[
-            {
-              "@font-face": {
-                fontFamily: "eUkraineRegular",
-                src: `url('${process.env.NEXT_PUBLIC_BASE_URL}/fonts/e-Ukraine/e-Ukraine-Regular.otf') format("opentype")`,
-                fontWeight: 500,
-                fontStyle: "normal",
-              },
-            },
-          ]}
-        />
-
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${process.env.NEXT_PUBLIC_BASE_URL}/meta/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${process.env.NEXT_PUBLIC_BASE_URL}/meta/favicon-32x32.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${process.env.NEXT_PUBLIC_BASE_URL}/meta/favicon-16x16.png`}
-        />
-        <link
-          rel="manifest"
-          href={`${process.env.NEXT_PUBLIC_BASE_URL}/manifest.json`}
-        />
-        <link
-          rel="mask-icon"
-          href={`${process.env.NEXT_PUBLIC_BASE_URL}/meta/safari-pinned-tab.svg`}
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <AppMeta />
       </Head>
       <I18nextProvider i18n={i18n}>
         <NotificationsProvider position="bottom-right">
