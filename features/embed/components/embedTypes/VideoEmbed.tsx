c138b5dcc5f17e2812881c8671acc6c66562951c
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-10-07 21:07:40 +0300

    Fix mp4 video crop

--- features/embed/components/embedTypes/VideoEmbed.tsx
+++ features/embed/components/embedTypes/VideoEmbed.tsx
@@ -1,9 +1,10 @@
+import { Box } from "@mantine/core";
 import { EmbedComponentType } from "../types";
 
 export const VideoEmbed: EmbedComponentType = ({ src }) => {
   return (
-    <video controls>
-      <source src={src} type="video/mp4" />
-    </video>
+    <Box component="video" controls sx={{ objectFit: "contain" }}>
+      <Box component="source" src={src} type="video/mp4" />
+    </Box>
   );
 };
