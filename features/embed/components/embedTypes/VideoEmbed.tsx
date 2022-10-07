d970f4aaf105bd9f2729f6dae7696e464d9896c1
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-10-07 21:13:19 +0300

    Fix objectFit for video

--- features/embed/components/embedTypes/VideoEmbed.tsx
+++ features/embed/components/embedTypes/VideoEmbed.tsx
@@ -3,7 +3,7 @@ import { EmbedComponentType } from "../types";
 
 export const VideoEmbed: EmbedComponentType = ({ src }) => {
   return (
-    <Box component="video" controls sx={{ objectFit: "contain" }}>
+    <Box component="video" controls style={{ objectFit: "contain" }}>
       <Box component="source" src={src} type="video/mp4" />
     </Box>
   );
