5d7040f14adc0d5f1df68587db9c86140aac8ef1
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-10-17 11:48:13 +0300

    Fix mentions

--- features/mentions/utils/mentions.ts
+++ features/mentions/utils/mentions.ts
@@ -1,6 +1,6 @@
 export const makeMentionAsLink = (text: string) => {
   return text.replace(
-    /(?:^|\s)\@([a-z0-9_]+)/gim,
-    `<a href="${process.env.NEXT_PUBLIC_BASE_URL}/user?username=$1">@$1</a>`
+    /(^|\s|>)\@([a-z0-9_]+)/gim,
+    `$1<a href="${process.env.NEXT_PUBLIC_BASE_URL}/user?username=$2">@$2</a>`
   );
 };
