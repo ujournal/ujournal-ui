998bb0fedb6ba7cb780d847fb26674c11423aab2
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-11-18 21:36:15 +0200

    Add missing files

---
+++ baza/mantine/theme.ts
@@ -0,0 +1,52 @@
+import { MantineTheme } from "@mantine/core";
+
+export const mantineTheme: Partial<MantineTheme> = {
+  fontFamily: "Roboto, sans-serif",
+  primaryShade: 6,
+  loader: "dots",
+  black: "#333",
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+  },
+  lineHeight: 1.5,
+  headings: {
+    fontFamily: "Roboto, sans-serif",
+    fontWeight: 500,
+    sizes: {
+      h1: {
+        fontSize: 26,
+        fontWeight: 500,
+        lineHeight: 1.5,
+      },
+      h2: {
+        fontSize: 20,
+        fontWeight: 500,
+        lineHeight: 1.5,
+      },
+      h3: {
+        fontSize: 18,
+        fontWeight: 500,
+        lineHeight: 1.5,
+      },
+      h4: {
+        fontSize: 18,
+        fontWeight: 500,
+        lineHeight: 1.5,
+      },
+      h5: {
+        fontSize: 18,
+        fontWeight: 500,
+        lineHeight: 1.5,
+      },
+      h6: {
+        fontSize: 18,
+        fontWeight: 500,
+        lineHeight: 1.5,
+      },
+    },
+  },
+};
